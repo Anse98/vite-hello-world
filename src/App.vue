e96abdcaae11a1851f8f491c22593550f0863734
Anse98 <lorenzo.anselmi98.com>
2023-11-03 18:39:05 +0100

    ADD-Bonus

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,25 @@
 <script>
+import AppHero from './components/AppHero.vue'
 export default {
+
+  components: {
+    AppHero,
+  },
+
   data() {
     return {
       title: "All'inizio Vite è bello macchinoso!",
     }
   }
 }
+
 </script>
 
 <template>
   <div class="title">
     <h1>{{ title }}</h1>
   </div>
+  <AppHero />
 </template>
 
 <style>
---
+++ src/components/AppHero.vue
@@ -0,0 +1,19 @@
+<script>
+
+</script>
+
+<template>
+  <div class="jumbotron">
+    <img src="../assets/img (134).webp">
+  </div>
+</template>
+
+<style>
+img {
+  max-width: 100%;
+}
+
+.jumbotron {
+  padding: 10px;
+}
+</style>
